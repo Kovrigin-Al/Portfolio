f8f7562cb960b22536e3ef5f103260ff504383d8
Kovrigin-Al <kovrigin.alexy@gmail.com>
2023-02-01 19:32:42 +0400

    change background photo

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -17,17 +17,17 @@ const Intro: FC<Props> = ({ id, reference }) => {
     <section
       ref={reference}
       id={id}
-      className="bg-[url('../assets/img/photo.jpeg')] flex-1 h-screen w-full bg-cover bg-top bg-no-repeat"
+      className="bg-[url('../assets/img/photo.jpeg')] flex-1 h-screen w-full bg-cover bg-top lg:bg-bottom bg-no-repeat"
     >
-      <div className=" h-full w-full bg-black bg-opacity-20 z-10">
-        <header className="prose h-full w-full p-10">
-          <h1 className=" text-white ">Hello!</h1>
-          <h1 className=" text-white">
+      <div className=" h-full w-full bg-black bg-opacity-30 z-10">
+        <header className="prose h-full w-full p-9 ">
+          <h1 className="text-3xl sm:text-5xl text-white ">Hello!</h1>
+          <h1 className="text-3xl sm:text-5xl text-white">
             I'm
             {text}
             {!isDone && <Cursor />}
           </h1>
-          <h2 className="mt-5 text-white ">
+          <h2 className="text-xl sm:text-2xl mt-5 text-white ">
             based in Tbilisi, Georgia
           </h2>
         </header>
